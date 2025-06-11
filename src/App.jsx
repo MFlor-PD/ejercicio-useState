e8a11f737e955cebd5dfb93abd7ecdffc290b120
Flor <mariaflorenciaperezd@gmail.com>
2025-06-11 18:52:53 +0200

    Version para cambiar el nombre del profe con click

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
- //Aquí tu código
+
+ const [name, setName] = useState('Sofia');
+ const [newName, setNewName] = useState('');
+
+ const names = ['Data', 'Reyes', 'Yolanda'];
+    return (
+        <div className="App">
+            <h1>Cambiale el nombre al Profesor</h1>
+            <h2>Teacher Name: {name}</h2>
+             <ul>
+        {names.map((prof, index) => (
+          <li key={index} onClick={() => setName(prof)} style={{cursor: 'pointer'}}>
+            {prof}
+          </li>
+        ))}
+      </ul>
+    </div>
+    )
 }
 
 export default App
