5e783f937e55c2903edd003ecf85bbce294d708f
Flor <mariaflorenciaperezd@gmail.com>
2025-06-11 19:39:29 +0200

    Versión con formulario para cambiar el nombre del profesor

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,30 @@ function App() {
  const [name, setName] = useState('Sofia');
  const [newName, setNewName] = useState('');
 
- const names = ['Data', 'Reyes', 'Yolanda'];
-    return (
-        <div className="App">
-            <h1>Cambiale el nombre al Profesor</h1>
-            <h2>Teacher Name: {name}</h2>
-             <ul>
-        {names.map((prof, index) => (
-          <li key={index} onClick={() => setName(prof)} style={{cursor: 'pointer'}}>
-            {prof}
-          </li>
-        ))}
-      </ul>
+ const changeNames = (e) => {
+    e.preventDefault();
+    if (newName !== '') {
+      setName(newName);
+      setNewName('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Cambiale el nombre al Profesor</h1>
+      <h2>Teacher Name: {name}</h2>
+      
+      <form onSubmit={changeNames}>
+        <input 
+          type="text" 
+          placeholder="Escribe el nuevo nombre" 
+          value={newName} 
+          onChange={(e) => setNewName(e.target.value)} 
+        />
+        <button type="submit">Cambiar Nombre</button>
+      </form>
     </div>
-    )
+  );
 }
 
-export default App
+export default App;
